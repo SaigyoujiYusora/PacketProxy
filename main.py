3d259d0f0c960b7ecf9ec4c88410f7f3450aa56c
KawaiiYusora <misaka09983@gmail.com>
2024-09-10 19:03:05 +0800

    修复了进入酒馆点炒饭导致的爆炸

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ class WebSocketManager:
         try:
             client_id = [*self.connections.keys()][-1] + 1
         except IndexError:
-            client_id = 0
+            client_id = 1
         self.connections[client_id] = websocket
 
         # 分配客户端标识名称
@@ -115,7 +115,7 @@ class WebSocketManager:
             client_name = f"客户端{client_id}"
         self.clients[client_name] = client_id
 
-    async def disconnect(self, websocket: WebSocket,data = None):
+    async def disconnect(self, websocket: WebSocket, data=None, inloop=False):
         # 获取客户端索引
         client_id = self.get_id(websocket)
         client_name = self.get_name(websocket)
@@ -124,6 +124,9 @@ class WebSocketManager:
         # 从clients中移除客户端
         del self.clients[client_name]
 
+        # 队列为空时直接返回
+        if self.event_queue.empty() and not data:
+            return
 
         # 尝试从空闲客户端列表获取一个客户端
         idle_client_ids = list(self.connections.keys())
@@ -133,22 +136,48 @@ class WebSocketManager:
         if idle_client_ids:
             new_client_id = random.choice(idle_client_ids)
         else:
-            new_client_id = random.choice(list(self.connections.keys()))
+            try:
+                new_client_id = random.choice(list(self.connections.keys()))
+            except IndexError:
+                logging.warning("没有可用的客户端")
+                new_client_id = -client_id
+
+        # 无客户端处理
+        if new_client_id == -client_id:
+            logging.warning("等待新的客户端连接")
+            for session, index in self.session_bind.items():
+                if index == client_id:
+                    self.session_bind[session] = new_client_id
+            while True:
+                try:
+                    new_client_id = random.choice(list(self.connections.keys()))
+                    client_id = -client_id
+                    break
+                except IndexError:
+                    logging.debug("等待客户端")
+                    await asyncio.sleep(5)
+                    continue
 
         # 将发送失败的数据转交给新的客户端
         if data:
             logging.warning("移交发送失败的数据包给新的客户端")
-            await self.send_data(data, client_id=new_client_id)
+            try:
+                await self.send_data(data, client_id=new_client_id)
+            except WebSocketDisconnect:
+                logging.warning("新的客户端连接失败")
+                new_client_id = await self.disconnect(self.connections[new_client_id], inloop=True)
         # 将客户端的sessionID交给新的客户端
         for session, index in self.session_bind.items():
             if index == client_id:
                 self.session_bind[session] = new_client_id
 
+        if inloop:
+            return new_client_id
 
     async def send_data(self, data_packet: Dict, client_name: str = None, client_id: int = None):
         if client_name:
             await self.connections[self.clients[client_name]].send_json(data_packet)
-        if client_id:
+        elif client_id:
             await self.connections[client_id].send_json(data_packet)
 
     async def broadcast(self, message: str):
@@ -186,6 +215,12 @@ class WebSocketManager:
         while True:
             try:
                 logging.info("发送心跳包")
+                # logging.debug(f"客户端: {self.get_name(websocket)}")
+                # logging.debug(f"客户端ID: {self.get_id(websocket)}")
+                # logging.debug(f"客户端连接: {websocket}")
+                if websocket not in list(self.connections.values()):
+                    logging.info(f"终止{websocket}心跳包")
+                    raise WebSocketDisconnect
                 await websocket.send_text("ping")
                 await asyncio.sleep(interval)
             except WebSocketDisconnect:
@@ -206,10 +241,10 @@ ws_manger = WebSocketManager()
 
 
 @app.get("/get_server_url/")
-def get_server_url():
+async def get_server_url():
     """
     获取主服务器的URL
-    :return:
+    :return: json
     """
     # 返回主服务器的URL
     return JSONResponse({"server_url": "ws://localhost:8000/ws"})
@@ -224,6 +259,10 @@ async def websocket_endpoint(websocket: WebSocket):
         try:
             if websocket not in list(ws_manger.connections.values()):
                 logging.info(f"终止{websocket}监听进程")
+                try:
+                    heartbeat_task.cancel()
+                except Exception as e:
+                    logging.error(f"终止{websocket}监听进程失败: {e}")
                 break
             cache = ws_manger.event_queue.peak()
 
@@ -232,6 +271,10 @@ async def websocket_endpoint(websocket: WebSocket):
                 # 分配随机客户端给sessionID
                 logging.info("未找到对应节点，分配客户端中")
                 await ws_manger.client_bind_session(cache.get("session"), ws_manger.random_choice_id())
+                continue
+            elif ws_manger.session_bind.get(cache.get("session")) == -1:
+                logging.info("客户端id为-1, 移除队列")
+                await ws_manger.event_queue.get()
                 continue
             # 如果当前客户端的sessionID和session_bind中的sessionID不匹配, 则跳过
             elif ws_manger.get_id(websocket) != ws_manger.session_bind.get(cache.get("session")):
@@ -243,21 +286,24 @@ async def websocket_endpoint(websocket: WebSocket):
             await asyncio.sleep(1)
             # logging.debug("在循环里2")
             continue
-        logging.debug("等待数据包")
         data = await ws_manger.event_queue.get()
         logging.debug("发送数据包")
         try:
             await websocket.send_json(data)
         except WebSocketDisconnect:
-            await ws_manger.disconnect(websocket,data)
+            await ws_manger.disconnect(websocket, data)
             logging.info("发送失败-->客户端断开连接")
         finally:
             heartbeat_task.cancel()
 
 
-
 @app.post("/send_data")
 async def send_data(data_packet: Dict):
+    """
+    接受数据包
+    :param data_packet: 格式参照api doc
+    :return:
+    """
     # 接收到数据包后将其放入事件队列
     check_list = ["header", "addr", "data", "session"]
     missing = []
@@ -285,6 +331,10 @@ async def send_data(data_packet: Dict):
 
 @app.get("/query_data")
 async def query_data():
+    """
+    查看事件队列
+    :return:
+    """
     # 打印整个事件队列
     data = {"latest_data": None, "queue": []}
     if not ws_manger.event_queue.empty():
@@ -304,6 +354,10 @@ async def ws_test(request: Request):
 
 @app.get("/get_online_clients")
 async def online_client():
+    """
+    获取在线客户端列表
+    :return:
+    """
     print(ws_manger.clients.keys())
     print(ws_manger.clients)
     print(ws_manger.connections)
